7651bc4455729cd371fadcc0e57741ffb27f0bcc
Jefferson Joel Gonzalez Montes <gm25001@esfe.agape.edu.sv>
2025-08-13 00:18:33 -0600

    Se modifico todo lo relacionado con personal

--- src/main/java/com/apexManagent/security/DatabaseWebSecurity.java
+++ src/main/java/com/apexManagent/security/DatabaseWebSecurity.java
@@ -12,8 +12,6 @@ import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-//import javax.sql.DataSource;
-
 @Configuration
 @EnableWebSecurity
 public class DatabaseWebSecurity {
