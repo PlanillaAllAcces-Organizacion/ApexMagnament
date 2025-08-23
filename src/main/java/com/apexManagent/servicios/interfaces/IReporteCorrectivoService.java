4a41117acec768842fd28b58ee1fba38e060fc26
Juan_Carlos_Vásquez_López <vl25001@esfe.agape.edu.sv>
2025-08-23 12:54:19 -0600

    Creacion de la vista reporteccorrectivo y modificacion en el main layout y el repositorio.

--- src/main/java/com/apexManagent/servicios/interfaces/IReporteCorrectivoService.java
+++ src/main/java/com/apexManagent/servicios/interfaces/IReporteCorrectivoService.java
@@ -2,18 +2,28 @@ package com.apexManagent.servicios.interfaces;
 
 import com.apexManagent.modelos.ReporteCorrectivo;
 import com.apexManagent.modelos.Solicitud;
+
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+
 public interface IReporteCorrectivoService {
-    
+
+    Page<ReporteCorrectivo> buscarReportes(String search, Short tipoMantenimiento,
+            org.springframework.data.domain.Pageable pageable);
+
     ReporteCorrectivo guardarReporte(ReporteCorrectivo reporte);
-    
+
     Optional<ReporteCorrectivo> obtenerPorSolicitud(Solicitud solicitud);
-    
+
     Optional<ReporteCorrectivo> obtenerPorSolicitudId(Integer solicitudId);
-    
+
     boolean existeReporteParaSolicitud(Integer solicitudId);
-    
-    ReporteCorrectivo crearReporteDesdeSolicitud(Solicitud solicitud, String observacion, 
-                                                Short tipoMantenimiento, Short estado); // Cambiado a Short
+
+    ReporteCorrectivo crearReporteDesdeSolicitud(Solicitud solicitud, String observacion,
+            Short tipoMantenimiento, Short estado); // Cambiado a Short
+
+    List<ReporteCorrectivo> findAll();
+
 }
