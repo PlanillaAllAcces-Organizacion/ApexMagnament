451f739738513cf0b90521e2157fa4d07f8bbd22
Juan_Carlos_Vásquez_López <vl25001@esfe.agape.edu.sv>
2025-08-23 19:22:22 -0600

    cambios minimos en servicios preventivo y personal y la entidad calendario preventivo.

--- src/main/java/com/apexManagent/servicios/implementaciones/PreventivoService.java
+++ src/main/java/com/apexManagent/servicios/implementaciones/PreventivoService.java
@@ -4,21 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import com.apexManagent.modelos.CalendarioPreventivo;
 import com.apexManagent.repositorio.IPreventivoRepository;
 import com.apexManagent.servicios.interfaces.IPreventivoService;
+import com.apexManagent.modelos.CalendarioPreventivo;
+import java.util.Optional;
 
 @Service
 public class PreventivoService implements IPreventivoService {
 
     @Autowired
-    public IPreventivoRepository preventivoRepository;
+    private IPreventivoRepository preventivoRepository;
 
     @Override
     public CalendarioPreventivo guardar(CalendarioPreventivo calendarioPreventivo) {
         return preventivoRepository.save(calendarioPreventivo);
-
     }
 
     @Override
@@ -31,4 +30,13 @@ public class PreventivoService implements IPreventivoService {
         return preventivoRepository.findByEstadoMantenimiento(estado, pageable);
     }
 
+    @Override
+    public Page<CalendarioPreventivo> buscarCalendarios(String search, Short estado, Pageable pageable) {
+        return preventivoRepository.buscarCalendarios(search, estado, pageable);
+    }
+
+    @Override
+    public Optional<CalendarioPreventivo> obtenerPorId(Integer id) {
+        return preventivoRepository.findById(id);
+    }
 }
